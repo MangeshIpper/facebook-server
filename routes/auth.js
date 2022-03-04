df2a22cb3bac42dec69151c2ff93e0b5024fb525
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-03-04 01:59:54 +0000

    Added update and delete user API.

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,11 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) res.status(404).json("User not found");
-    res.status(200).json("Login Successful");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) res.status(400).json("Wrong password");
+
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
