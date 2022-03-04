e2f12ea834d05b66fa6af3cde4ae1fbf859b0e82
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-03-04 02:06:52 +0000

    Added post model and post create, delete, update, get, like/dislike, timeline API's.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const auth = require("./auth");
 const users = require("./users");
+const posts = require("./posts");
 
 router.use('/auth', auth);
 router.use('/user', users);
+router.use('/posts', posts);
 
 
 
